5e1e45588726fc77b8139d0a3347b84a7c8a0105
jiyoung <jjiyoung0421@gmail.com>
2024-03-21 04:41:17 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -25,21 +25,54 @@ const choice = {
   },
 };
 function App() {
-  const [useSelect, setUserSelect] = useState(null)
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
   const play = (userChoice) => {
-    setUserSelect(choice[userChoice])
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value", randomItem);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    // user == computer tie
+    // user == rock, computer == "scissors" user 이긴거지
+    // user == "rock" computer == paper   user 진거지
+    // user == scissors computer paper    user 이긴거지
+    // user == scissors computer rock     user 진거지
+    // user == paper computer rock   user 이긴거지
+    // user paper computer scissors user 진거지
+
+    if (user.name == computer.name) {
+      return "tie";
+    } else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "win" : "lose";
+    else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "win" : "lose";
+    else if (user.name == "Paper")
+      return computer.name == "Rock" ? "win" : "lose";
   };
   return (
     <div>
       <div className="main">
-        <Box title="you" item={useSelect} />
-        {/* <Box title="computer" /> */}
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
-      ;
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={ () => play("rock")}>바위</button>
-        <button onClick={() =>  play("paper")}>보</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
